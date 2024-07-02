34d17c076b1f9dbda27f95d6c1f062f665515bb3
Sohailcode <100mohdsohail@gmail.com>
2024-07-02 14:53:23 +0530

    remove authentication bcz of render website not accesing token

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { getAllMessages, sendMessage } from '../controller/messageController.js';
-import { isAdminAuthenticated, isPatienAuthenticated } from "../middlewares/auth.js";
+// import { getAllMessages, sendMessage } from '../controller/messageController.js';
+// import { isAdminAuthenticated, isPatienAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
 
-router.post('/post',isPatienAuthenticated,postAppointment);
-router.get('/getAll',isAdminAuthenticated,getAllAppointments);
-router.put('/update/:id',isAdminAuthenticated,updateAppointmentStatus);
-router.delete('/delete/:id',isAdminAuthenticated,deleteAppointment);
+router.post('/post',postAppointment);
+router.get('/getAll',getAllAppointments);
+router.put('/update/:id',updateAppointmentStatus);
+router.delete('/delete/:id',deleteAppointment);
+// router.post('/post',isPatienAuthenticated,postAppointment);
+// router.get('/getAll',isAdminAuthenticated,getAllAppointments);
+// router.put('/update/:id',isAdminAuthenticated,updateAppointmentStatus);
+// router.delete('/delete/:id',isAdminAuthenticated,deleteAppointment);
 
 export default router;
--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -5,13 +5,22 @@ import { isAdminAuthenticated,isPatienAuthenticated } from '../middlewares/auth.
 
 router.post('/patient/register',patientRegister);
 router.post('/login',login);
-router.post('/admin/addnew',isAdminAuthenticated,addNewAdmin);
+router.post('/admin/addnew',addNewAdmin);
 router.get('/doctors',getallDoctor);
-router.get('/admin/me',isAdminAuthenticated,getUserDetails);
-router.get('/patient/me',isPatienAuthenticated,getUserDetails);
-router.get('/admin/logout',isAdminAuthenticated,adminLogout);
-router.get('/patient/logout',isPatienAuthenticated,patientLogout);
-router.post('/doctor/addnew',isAdminAuthenticated,addNewDoctor);
+router.get('/admin/me',getUserDetails);
+router.get('/patient/me',getUserDetails);
+router.get('/admin/logout',adminLogout);
+router.get('/patient/logout',patientLogout);
+router.post('/doctor/addnew',addNewDoctor);
+// router.post('/patient/register',patientRegister);
+// router.post('/login',login);
+// router.post('/admin/addnew',isAdminAuthenticated,addNewAdmin);
+// router.get('/doctors',getallDoctor);
+// router.get('/admin/me',isAdminAuthenticated,getUserDetails);
+// router.get('/patient/me',isPatienAuthenticated,getUserDetails);
+// router.get('/admin/logout',isAdminAuthenticated,adminLogout);
+// router.get('/patient/logout',isPatienAuthenticated,patientLogout);
+// router.post('/doctor/addnew',isAdminAuthenticated,addNewDoctor);
 
 
 export default router;
